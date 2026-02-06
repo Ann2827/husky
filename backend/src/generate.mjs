6dd0313a8b63d6612e86d0507fbb6785d3ffeeab
Bystrova Ann <ann.bystrova96@mail.ru>
2026-02-06 18:58:30 +0300

    feat: init

---
+++ backend/src/generate.mjs
@@ -0,0 +1,10 @@
+import * as fs from 'node:fs';
+
+if (!fs.existsSync('../../generated')) {
+    fs.mkdirSync('../../generated', { recursive: true });
+}
+try {
+    fs.writeFileSync('../../generated/result.txt', `${new Date().toTimeString()}`);
+} catch (err) {
+    console.error('Error writing file:', err);
+}
