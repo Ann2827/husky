8d5103b538ffd572b2e1cbdcf2a8713d0f02fa87
Bystrova Ann <ann.bystrova96@mail.ru>
2026-02-06 19:04:51 +0300

    feat: add generate

--- backend/src/generate.mjs
+++ backend/src/generate.mjs
@@ -1,10 +1,11 @@
 import * as fs from 'node:fs';
 
 if (!fs.existsSync('../../generated')) {
-    fs.mkdirSync('../../generated', { recursive: true });
+    fs.mkdirSync('../generated', { recursive: true });
 }
+
 try {
-    fs.writeFileSync('../../generated/result.txt', `${new Date().toTimeString()}`);
+    fs.writeFileSync('../generated/result.txt', new Date().toTimeString());
 } catch (err) {
     console.error('Error writing file:', err);
 }
